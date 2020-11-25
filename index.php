46b48d9524317765cfa32dd0b2006597ae656f07
Jannis Otto <jannis.otto@itc-studenten.de>
2020-11-25 21:15:55 +0100

    Index.php anpssung

--- index.php
+++ index.php
@@ -1,15 +1,49 @@
 <!DOCTYPE html>
 <html lang="de">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Öl Imperium</title>
-</head>
-<body>
-  <form action="">
 
-  </form>
-Hello WWW
-test
-</body>
+  <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oil Imperium</title>
+  </head>
+
+  <body>
+  <!-- Full Page Intro -->
+  <div class="view" style="background-image: url('Oil.png'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+    <!-- Mask & flexbox options-->
+    <div class="mask rgba-black-light align-items-center">
+      <!-- Content -->
+      <div class="container align-middle">
+        <!--Grid row-->
+        <div class="row">
+          <!--Grid column-->
+          <div class="col-md-12 mb-4 text-white text-center">
+            <h1 class="h1-reponsive text-white mb-0 pt-md-5 pt-5 wow fadeInDown" data-wow-delay="0.3s"><strong>Oil Imperium</strong></h1>
+            <hr class="hr-light my-4 wow fadeInDown" data-wow-delay="0.4s">
+            <h5 class="text-uppercase mb-4 text-white wow fadeInDown" data-wow-delay="0.4s"><strong>Spielmenü</strong></h5>
+            <a class="btn btn-outline-light wow fadeInDown" data-wow-delay="0.4s">Create Game</a>
+            <a class="btn btn-outline-light wow fadeInDown" data-wow-delay="0.4s">Join Game</a>
+          </div>
+          <!--Grid column-->
+        </div>
+        <!--Grid row-->
+      </div>
+      <!-- Content -->
+    </div>
+    <!-- Mask & flexbox options-->
+  </div>
+  <!-- Full Page Intro -->
+  </body>
+
+<style>
+  html,body,.view{
+    height: 100%;
+  }
+  *{
+    font-family:"Century Gothic";
+  }
+</style>
+
 </html>
