4d40b1fa3286525ec7d37f70986630fee7240f69
MTim0815 <tim@assicona.de>
2020-12-29 17:58:38 +0100

    Newer WSS

--- Socket/Server/websocketUser.php
+++ Socket/Server/websocketUser.php
@@ -6,6 +6,10 @@ class WebSocketUser {
   public $id;
   public $header = array();
   public $handshake = false;
+  //long msg Handling
+  public $multipleFrames;
+  public $partialBuffer; 
+  public $partialMsg; 
 
   function __construct($id, $socket) {
     $this->id = $id;
