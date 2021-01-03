a74af743fd4553430edd702e3fbd76605b025adc
Hydrocynus <tobias.ulmer@itc-studenten.de>
2021-01-03 23:32:28 +0100

    Ein neues Socket Objekt verbindet sich jetzt automatisch mit dem Socket und startet ihn, wenn er offline ist.

--- js/classes/Socket.js
+++ js/classes/Socket.js
@@ -19,10 +19,27 @@ class Socket {
     this.port = port;
     this.maxTryCount = 3;
     let url = "ws://" + host + ":" + port;
+    this.openWebSocket(url);
+  }
 
+  /**
+   * 
+   * @author Tim Tobias
+   * @version 03.01.2021
+   * @since 03.01.2021
+   * @param {String} url 
+   * @returns {void}
+   */
+  openWebSocket(url) {
+    if (this.maxTryCount == 0) return;
+    this.maxTryCount--;
     this.socket = new WebSocket(url);
-    this.socket.onerror = (e) => console.error("WebSocket Error: ", e);
-    this.socket.onopen = (e) => console.debug("WebSocket connected: ", e);
+    this.socket.onerror = () => {
+      this.startWebSocket();
+      Utils.delay(1000);
+      this.openWebSocket(url);
+    }
+    this.socket.onopen  = (e) => console.debug("WebSocket connected on " + url);
   }
 
   /**
@@ -34,7 +51,7 @@ class Socket {
    * @param {Number|String} [port] 
    * @returns {void}
    */
-  static async startWebSocket(host = this.host, port = this.port) {
+  async startWebSocket(host = this.host, port = this.port) {
     const data = "fnc=startWebSocket"
                + "&hst=" + encodeURIComponent(host)
                + "&prt=" + encodeURIComponent(port);
