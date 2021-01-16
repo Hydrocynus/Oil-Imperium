6343e0a5a641a2134ef03ae12586fb4439fe58a2
Hydrocynus <tobias.ulmer@itc-studenten.de>
2021-01-16 17:57:15 +0100

    load weil test oder so

--- game/game.js
+++ game/game.js
@@ -3,11 +3,13 @@ Xhr.rootPath = "..";
 //Anzeigen des LobbyCodes
 d3.select("#code").text(localStorage.getItem("code") || "----");
 
-// Socket verbinden
-let gc = new GameControl();
-
 //Frabe aendern durch ein Lable
 function colorchange(element){
-    console.log(element);
-    d3.select(element.parentNode).select('label').style('background-color',element.value);
+  console.log(element);
+  d3.select(element.parentNode).select('label').style('background-color',element.value);
 };
+
+function load() {
+  // Socket verbinden
+  gc = new GameControl();
+}
