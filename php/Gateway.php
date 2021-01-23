069466f96b81cb8de06265ea8a49ddb1d4509831
Hydrocynus <tobias.ulmer@itc-studenten.de>
2021-01-23 16:13:12 +0100

    Methodenkommentare

--- php/Gateway.php
+++ php/Gateway.php
@@ -9,7 +9,7 @@ $_POST['fnc']();
 if (!$buffer_closed) closeBuffer();
 
 /**
- * 
+ * Startet einen neuen Websocket Server.
  * @author Tobias
  * @version 03.01.2021
  * @since 31.12.2020
@@ -51,7 +51,7 @@ function closeBuffer() {
   $buffer_closed = true;
   $size = ob_get_length();
   header("Content-Length: $size");
-  ob_end_flush();     // Strange behaviour, will not work
-  flush();            // Unless both are called !
+  @ob_end_flush();     // Strange behaviour, will not work
+  @flush();            // Unless both are called !
   @ob_end_clean();
 }
