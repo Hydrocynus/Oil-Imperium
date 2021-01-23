74ac96ad9d8230374a07462c77668d4920a405f1
Hydrocynus <tobias.ulmer@itc-studenten.de>
2021-01-23 17:01:49 +0100

    Handlerfunktionen für Änderungen der Spieler

--- game/game.js
+++ game/game.js
@@ -1,15 +1,26 @@
 Xhr.rootPath = "..";
+// Socket verbinden
+gc = new GameControl();
 
 //Anzeigen des LobbyCodes
 d3.select("#code").text(localStorage.getItem("code") || "----");
 
 //Frabe aendern durch ein Lable
-function colorchange(element){
-  console.log(element);
+function colorChange(element){
   d3.select(element.parentNode).select('label').style('background-color',element.value);
+  let value = { color: element.value };
+  value     = JSON.stringify(value);
+  gc.sendInstruction("userChange", value);
 };
 
-function load() {
-  // Socket verbinden
-  gc = new GameControl();
+function nameChange(element) {
+  let value = { name: element.value };
+  value     = JSON.stringify(value);
+  gc.sendInstruction("userChange", value);
+}
+
+function readyChange(element) {
+  let value = { ready: element.checked };
+  value     = JSON.stringify(value);
+  gc.sendInstruction("userChange", value);
 }
