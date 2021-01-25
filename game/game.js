d6626cfbfee1255b98d67c757b1088df401a8841
Hydrocynus <tobias.ulmer@itc-studenten.de>
2021-01-25 23:00:46 +0100

    Instruction value ist jetzt nicht mehr zwangsweise String

--- game/game.js
+++ game/game.js
@@ -9,18 +9,18 @@ d3.select("#code").text(localStorage.getItem("code") || "----");
 function colorChange(element){
   d3.select(element.parentNode).select('label').style('background-color',element.value);
   let value = { id: localStorage.getItem('ID'), color: element.value };
-  value     = JSON.stringify(value);
+  // value     = JSON.stringify(value);
   gc.sendInstruction("userChange", value);
 };
 
 function nameChange(element) {
   let value = { id: localStorage.getItem('ID'), name: element.value };
-  value     = JSON.stringify(value);
+  // value     = JSON.stringify(value);
   gc.sendInstruction("userChange", value);
 }
 
 function readyChange(element) {
   let value = { id: localStorage.getItem('ID'), ready: element.checked };
-  value     = JSON.stringify(value);
+  // value     = JSON.stringify(value);
   gc.sendInstruction("userChange", value);
 }
