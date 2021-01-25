b4511158648d1ca35a5e8b83858789a6bd25652d
Hydrocynus <tobias.ulmer@itc-studenten.de>
2021-01-25 21:36:50 +0100

    ID wird jetzt bei Änderungen mit übertragen

--- game/game.js
+++ game/game.js
@@ -8,19 +8,19 @@ d3.select("#code").text(localStorage.getItem("code") || "----");
 //Frabe aendern durch ein Lable
 function colorChange(element){
   d3.select(element.parentNode).select('label').style('background-color',element.value);
-  let value = { color: element.value };
+  let value = { id: localStorage.getItem('ID'), color: element.value };
   value     = JSON.stringify(value);
   gc.sendInstruction("userChange", value);
 };
 
 function nameChange(element) {
-  let value = { name: element.value };
+  let value = { id: localStorage.getItem('ID'), name: element.value };
   value     = JSON.stringify(value);
   gc.sendInstruction("userChange", value);
 }
 
 function readyChange(element) {
-  let value = { ready: element.checked };
+  let value = { id: localStorage.getItem('ID'), ready: element.checked };
   value     = JSON.stringify(value);
   gc.sendInstruction("userChange", value);
 }
