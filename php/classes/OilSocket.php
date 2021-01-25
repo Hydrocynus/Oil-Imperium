06d964cab5ceb7443c069dadf12a9211d9158f8d
MTim0815 <tim@assicona.de>
2021-01-25 23:27:29 +0100

    json ist schon irgendwo lost

--- php/classes/OilSocket.php
+++ php/classes/OilSocket.php
@@ -6,32 +6,30 @@ class OilSocket extends Socket {
 
   protected function onMessage($user, $msg) {
     
-    try { $msg = json_decode($msg); }
+    try { $msg = json_decode($msg, true); }
     catch (Exception $e) { return ; }
     $cmd = $msg[0];
-    $msg = $msg[1];
+    $val = $msg[1];
     
     switch($cmd) {
       case "PING": 
         return;
-      case "TEST":
-        $this->broadcastInstruction($cmd, $msg);
+      case "broadcast":
+        $this->broadcastInstruction($cmd, $val);
         break;
-      case "LogUser": 
+      case "userList": 
         $this->userList();
         break;
       case "userChange":
       case "userAdd":
-        $msg = json_decode($msg, true);
-        $user->setUserInfo($msg);
-        $msg = json_encode($msg);
-        $this->broadcastInstruction($cmd, $msg);
+        $user->setUserInfo($val);
+        $this->broadcastInstruction($cmd, $val);
         break;
       case "getPlayerlist": 
         $this->sendPlayerlist($user);
         break; 
       default: 
-        LogHandler::writeLog("Server got: ". $msg);
+        LogHandler::writeLog("Server got: ". $val);
         LogHandler::writeLog("from: ". $user->id);
         break;
     }
@@ -41,9 +39,9 @@ class OilSocket extends Socket {
 
   protected function onConnection($user) {
     $info = $user->getUserInfo();
-    $msg = json_encode(["userSelf", json_encode($info)]);
+    $msg = json_encode(["userSelf", $info]);
     $this->send($user, $msg);
-    $this->broadcastInstruction("userAdd", json_encode($info));
+    $this->broadcastInstruction("userAdd", $info);
   }
 
   protected function onClose($user) {
@@ -61,8 +59,8 @@ class OilSocket extends Socket {
    * @param string $msg Wert der Anweisung.
    * @return void
    */
-  public function broadcastInstruction($cmd, $msg) {
-    $msg = json_encode([$cmd, $msg]);
+  public function broadcastInstruction($cmd, $val) {
+    $msg = json_encode([$cmd, $val]);
     $this->broadcast($msg);
   }
 }
