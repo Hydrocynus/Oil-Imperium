35d4c0a4868eeb5ea3be155e3eee17fffd17f462
MTim0815 <tim@assicona.de>
2021-01-25 23:28:12 +0100

    Danke Jannis

--- php/classes/Utils.php
+++ php/classes/Utils.php
@@ -152,7 +152,7 @@ class Utils {
    * Random Hex wert für einen Random Farbcode
    * @author Jannis
    * @since 25.01.2021
-   * @return HexColorCode
+   * @return string HexColorCode
    */
   public static function randomHexColorCode() {
     $rand = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f');
